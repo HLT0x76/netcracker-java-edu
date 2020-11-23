8054fa287c234affc06c9c15062060116010d78c
HLT0x76 <c.tyutyunnikov@gmail.com>
2020-11-23 03:45:14 +0300

    the field type contract changed to ContractRepository class

--- src/main/java/com/netcracker/edu/Customers/Customer.java
+++ src/main/java/com/netcracker/edu/Customers/Customer.java
@@ -1,6 +1,7 @@
 package com.netcracker.edu.Customers;
 
 import com.netcracker.edu.Contracts.Contract;
+import com.netcracker.edu.Repository.ContractsRepository;
 import java.time.LocalDate;
 import java.time.Period;
 
@@ -12,13 +13,14 @@ import lombok.AccessLevel;
 @Setter(AccessLevel.PROTECTED)
 public class Customer {
 
+
     private final int id;
     private Gender gender;
     private LocalDate dateOfBirth;
     private final int age;
     private String fullName;
     private String passport;
-    private Contract contract;
+    private ContractsRepository contracts;
 
     /**
      * This class provides basic Customer functionality
@@ -26,10 +28,25 @@ public class Customer {
      * @param dateOfBirth the date of birth
      * @param fullName full name of a Costumer
      * @param passport the passport string
-     * @param contract the contract object, inherited from {@link Contract} class
+     * @param contracts object, which extends {@link Contract} abstract or {@link ContractsRepository} itself
      */
 
     public Customer(int id,
+                    LocalDate dateOfBirth,
+                    Gender gender,
+                    String fullName,
+                    String passport,
+                    ContractsRepository contracts) {
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.fullName = fullName;
+        this.passport = passport;
+        this.contracts = contracts;
+        this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+        public Customer(int id,
                     LocalDate dateOfBirth,
                     Gender gender,
                     String fullName,
@@ -40,7 +57,8 @@ public class Customer {
         this.gender = gender;
         this.fullName = fullName;
         this.passport = passport;
-        this.contract = contract;
+        this.contracts = new ContractsRepository();
+        this.contracts.add(contract);
         this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
     }
 
