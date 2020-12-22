637849e88d3fadc1393c262051d2fafbd0059c1f
HLT0x76 <c.tyutyunnikov@gmail.com>
2020-12-22 21:15:14 +0300

    linkWith and checkNext methods deleted

--- src/main/java/com/netcracker/edu/validators/Validator.java
+++ src/main/java/com/netcracker/edu/validators/Validator.java
@@ -1,28 +1,13 @@
 package com.netcracker.edu.validators;
 
 /**
- * Abstract class, which implements "chain of responsibility" pattern.
- * This allows us to chain different classes,
- * which extends {@code Validator}, and perform complex validation algorithms.
+ * Validator abstract class for a concrete classes, which
+ * will extend it and perform different validations.
  *
  * @param <T> object, which fields or functions results will be checked
  */
 public abstract class Validator<T> {
 
-  private Validator<T> next;
-
-  public Validator<T> linkWith(Validator<T> next) {
-    this.next = next;
-    return next;
-  }
-
   public abstract ValidationReport check(T object);
 
-  protected ValidationReport checkNext(T object) {
-    if (next == null) {
-      return new ValidationReport(ValidationStatus.OK);
-    } else {
-      return next.check(object);
-    }
-  }
 }
