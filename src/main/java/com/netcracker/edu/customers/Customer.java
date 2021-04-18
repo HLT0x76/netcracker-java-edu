f892aa8ef6b189957889d00ef2e7a0687cf9db36
HLT0x76 <c.tyutyunnikov@gmail.com>
2021-04-18 20:34:42 +0300

    JAXB annotations added

--- src/main/java/com/netcracker/edu/customers/Customer.java
+++ src/main/java/com/netcracker/edu/customers/Customer.java
@@ -2,22 +2,27 @@ package com.netcracker.edu.customers;
 
 import java.time.LocalDate;
 import java.time.Period;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+
+import com.netcracker.edu.repositoryHandlers.xml.utils.DateAdapter;
+import com.netcracker.edu.repositoryHandlers.xml.utils.GenderAdapter;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
  * Basic customer class implementation.
  */
-@Getter
-@Setter(AccessLevel.PROTECTED)
+@XmlRootElement(name = "customer")
 public class Customer {
 
-  private final int id;
+  @XmlAttribute
+  private int id;
   private static int nextId = 0;
   private Gender gender;
   private LocalDate dateOfBirth;
-  private final int age;
+  private int age;
   private String fullName;
   private String passport;
 
@@ -28,7 +33,6 @@ public class Customer {
    * @param fullName full name of a Costumer
    * @param passport the passport string
    */
-
   public Customer(LocalDate dateOfBirth,
                   Gender gender,
                   String fullName,
@@ -41,5 +45,72 @@ public class Customer {
     this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
   }
 
+    public Customer(Integer id,
+                    LocalDate dateOfBirth,
+                    Gender gender,
+                    String fullName,
+                    String passport) {
+    this.id = id;
+    this.dateOfBirth = dateOfBirth;
+    this.gender = gender;
+    this.fullName = fullName;
+    this.passport = passport;
+    this.age = Period.between(dateOfBirth, LocalDate.now()).getYears();
+  }
+
+  public Customer() {
+  }
+
+  public Gender getGender() {
+    return gender;
+  }
+
+  @XmlElement
+  @XmlJavaTypeAdapter(GenderAdapter.class)
+  public void setGender(Gender gender) {
+    this.gender = gender;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  @XmlElement
+  @XmlJavaTypeAdapter(DateAdapter.class)
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getPassport() {
+    return passport;
+  }
+
+  public void setPassport(String passport) {
+    this.passport = passport;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
 }
 
